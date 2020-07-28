5b7ff0488496bb0baa1451c24f910aabb1a31c53
Info qmic <infoqmic@Sajjeels-MacBook-Pro-2.local>
2020-07-28 11:32:21 +0300

    Added the keys

---
+++ app/src/main/java/com/example/coroutinewrapper/AndApp.kt
@@ -0,0 +1,19 @@
+package com.example.coroutinewrapper
+
+import android.app.Application
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
+
+class AndApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        AppCenter.start(
+            this, "7ab0e147-88b6-4fcc-89ad-e5ab37e90821",
+            Analytics::class.java, Crashes::class.java
+        )
+    }
+}
